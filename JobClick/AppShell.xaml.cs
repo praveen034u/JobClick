319bb3f74f94afccc0daadfa2ccc534491bb2b4b
praveen srivastava <praveen034u@gmail.com>
2025-06-11 23:50:40 -0400

    updated

---
+++ JobClick/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace JobClick
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ JobClick/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace JobClick
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
